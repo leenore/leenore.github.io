04c3d0014c5c941318b495795e509022499d13bb
Eugene Kalenik <evgeny.kalenik@globant.com>
2021-08-04 10:04:50 +0300

    initial commit in index branch

--- src/components/PageHeader.js
+++
@@ -1,9 +0,0 @@
-export const PageHeader = ({ title }) => {
-  return (
-    <div className='page-header'>
-      <div className='container'>
-        <h2>{title}</h2>
-      </div>
-    </div>
-  );
-};
